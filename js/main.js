dc9a15822af75a921daa44557311406999505045
LaggerIsME <abajsadykov2003@gmail.com>
2022-02-22 22:19:34 +0600

    Mobile version

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-$(document).ready(function() {
-  //анимация обложек
+  //анимация обложек ПК
   $('.post-punk').hover(function() {
     $('#main').css({
       "background-image": "url('media/post-punk.jpg')",
@@ -34,9 +33,39 @@ $(document).ready(function() {
   //анимация текста
   $('.music').mouseover(function() {
     $(this).css("padding-left", "10px");
-  })
+  });
   $('.music').mouseout(function() {
     $(this).css("padding-left", "0px");
-  })
+  });
 
-});
+  //анимация обложек мобильной версии
+  $('.m_post-punk').hover(function() {
+    $('#main').css({
+      "background-image": "url('media/m_post-punk.jpg')",
+      "transition-delay": "0.5s"
+    });
+  });
+  $('.m_pop-punk').hover(function() {
+    $('#main').css({
+      "background-image": "url('media/m_pop-punk.jpg')",
+      "transition-delay": "0.5s"
+    });
+  });
+  $('.m_classic').hover(function() {
+    $('#main').css({
+      "background-image": "url('media/m_classic.jpg')",
+      "transition-delay": "0.5s"
+    });
+  });
+  $('.m_indi').hover(function() {
+    $('#main').css({
+      "background-image": "url('media/m_indi.jpg')",
+      "transition-delay": "0.5s"
+    });
+  });
+  $('.m_metal').hover(function() {
+    $('#main').css({
+      "background-image": "url('media/m_metal.jpg')",
+      "transition-delay": "0.5s"
+    });
+  });
