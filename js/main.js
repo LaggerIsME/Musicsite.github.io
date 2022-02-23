5666c85157e1e30f724c5c0e16c3d05efe8a428a
LaggerIsME <abajsadykov2003@gmail.com>
2022-02-23 20:04:29 +0600

    Music on the site

--- js/main.js
+++ js/main.js
@@ -1,33 +1,118 @@
+  //три плеера на сайте
+  var audio1 = $("#player1");
+  var audio2 = $("#player2");
+  var audio3 = $("#player3");
+
   //анимация обложек ПК
-  $('.post-punk').hover(function() {
+  $('.post-punk').click(function() {
     $('#main').css({
       "background-image": "url('media/post-punk.jpg')",
       "transition-delay": "0.5s"
     });
+    //Смена треков
+    //Истоничк первой песни
+    $("#source1").attr("src", "audio/post-punk/Виктор Цой_Кино - Песня Без Слов.mp3");
+    /****************/
+    audio1[0].pause();
+    audio1[0].load(); //suspends and restores all audio element
+    //Истоничк второй песни
+    $("#source2").attr("src", "audio/post-punk/Июльские Дни - Мой город будет стоять.mp3");
+    /****************/
+    audio2[0].pause();
+    audio2[0].load(); //suspends and restores all audio element
+    //Истоничк третьей песни
+    $("#source3").attr("src", "audio/post-punk/Янка - По трамвайным рельсам.mp3");
+    /****************/
+    audio3[0].pause();
+    audio3[0].load(); //suspends and restores all audio element
   });
-  $('.pop-punk').hover(function() {
+  $('.pop-punk').click(function() {
     $('#main').css({
       "background-image": "url('media/pop-punk.jpg')",
       "transition-delay": "0.5s"
     });
+    //Смена треков
+    //Истоничк первой песни
+    $("#source1").attr("src", "audio/pop-punk/Соседи Стерпят - Девочка панк.mp3");
+    /****************/
+    audio1[0].pause();
+    audio1[0].load(); //suspends and restores all audio element
+    //Истоничк второй песни
+    $("#source2").attr("src", "audio/pop-punk/Соседи Стерпят - Пепел мой последний друг.mp3");
+    /****************/
+    audio2[0].pause();
+    audio2[0].load(); //suspends and restores all audio element
+    //Истоничк третьей песни
+    $("#source3").attr("src", "audio/pop-punk/Соседи Стерпят feat. Ai Mori - Влюбилась (руки на шее) [feat. Ai Mori].mp3");
+    /****************/
+    audio3[0].pause();
+    audio3[0].load(); //suspends and restores all audio element
   });
-  $('.classic').hover(function() {
+  $('.classic').click(function() {
     $('#main').css({
       "background-image": "url('media/classic.jpg')",
       "transition-delay": "0.5s"
     });
+    //Смена треков
+    //Истоничк первой песни
+    $("#source1").attr("src", "audio/classic/Звери - Районы-кварталы.mp3");
+    /****************/
+    audio1[0].pause();
+    audio1[0].load(); //suspends and restores all audio element
+    //Истоничк второй песни
+    $("#source2").attr("src", "audio/classic/Звери - Рома, извини.mp3");
+    /****************/
+    audio2[0].pause();
+    audio2[0].load(); //suspends and restores all audio element
+    //Истоничк третьей песни
+    $("#source3").attr("src", "audio/classic/Звери - Южная ночь.mp3");
+    /****************/
+    audio3[0].pause();
+    audio3[0].load(); //suspends and restores all audio element
   });
-  $('.indi').hover(function() {
+  $('.indi').click(function() {
     $('#main').css({
       "background-image": "url('media/indi.jpg')",
       "transition-delay": "0.5s"
     });
+    //Смена треков
+    //Истоничк первой песни
+    $("#source1").attr("src", "audio/indi/Sati Akura - Otome Dissection.mp3");
+    /****************/
+    audio1[0].pause();
+    audio1[0].load(); //suspends and restores all audio element
+    //Истоничк второй песни
+    $("#source2").attr("src", "audio/indi/Sati Akura - The Vampire.mp3");
+    /****************/
+    audio2[0].pause();
+    audio2[0].load(); //suspends and restores all audio element
+    //Истоничк третьей песни
+    $("#source3").attr("src", "audio/indi/Sati Akura[Vocaloid RUS] - HIBANA .mp3");
+    /****************/
+    audio3[0].pause();
+    audio3[0].load(); //suspends and restores all audio element
   });
-  $('.metal').hover(function() {
+  $('.metal').click(function() {
     $('#main').css({
       "background-image": "url('media/metal.jpg')",
       "transition-delay": "0.5s"
     });
+    //Смена треков
+    //Истоничк первой песни
+    $("#source1").attr("src", "audio/metal/Волшебники Изумрудного Города - Не вам меня судить.mp3");
+    /****************/
+    audio1[0].pause();
+    audio1[0].load(); //suspends and restores all audio element
+    //Истоничк второй песни
+    $("#source2").attr("src", "audio/metal/Волшебники Изумрудного Города - Не такой как все.mp3");
+    /****************/
+    audio2[0].pause();
+    audio2[0].load(); //suspends and restores all audio element
+    //Истоничк третьей песни
+    $("#source3").attr("src", "audio/metal/Волшебники Изумрудного Города - Устоять.mp3");
+    /****************/
+    audio3[0].pause();
+    audio3[0].load(); //suspends and restores all audio element
   });
 
   //анимация текста
@@ -39,33 +124,113 @@
   });
 
   //анимация обложек мобильной версии
-  $('.m_post-punk').hover(function() {
+  $('.m_post-punk').click(function() {
     $('#main').css({
       "background-image": "url('media/m_post-punk.jpg')",
       "transition-delay": "0.5s"
     });
+    //Смена треков
+    //Истоничк первой песни
+    $("#source1").attr("src", "audio/post-punk/Виктор Цой_Кино - Песня Без Слов.mp3");
+    /****************/
+    audio1[0].pause();
+    audio1[0].load(); //suspends and restores all audio element
+    //Истоничк второй песни
+    $("#source2").attr("src", "audio/post-punk/Июльские Дни - Мой город будет стоять.mp3");
+    /****************/
+    audio2[0].pause();
+    audio2[0].load(); //suspends and restores all audio element
+    //Истоничк третьей песни
+    $("#source3").attr("src", "audio/post-punk/Янка - По трамвайным рельсам.mp3");
+    /****************/
+    audio3[0].pause();
+    audio3[0].load(); //suspends and restores all audio element
   });
-  $('.m_pop-punk').hover(function() {
+  $('.m_pop-punk').click(function() {
     $('#main').css({
       "background-image": "url('media/m_pop-punk.jpg')",
       "transition-delay": "0.5s"
     });
+    //Смена треков
+    //Истоничк первой песни
+    $("#source1").attr("src", "audio/pop-punk/Соседи Стерпят - Девочка панк.mp3");
+    /****************/
+    audio1[0].pause();
+    audio1[0].load(); //suspends and restores all audio element
+    //Истоничк второй песни
+    $("#source2").attr("src", "audio/pop-punk/Соседи Стерпят - Пепел мой последний друг.mp3");
+    /****************/
+    audio2[0].pause();
+    audio2[0].load(); //suspends and restores all audio element
+    //Истоничк третьей песни
+    $("#source3").attr("src", "audio/pop-punk/Соседи Стерпят feat. Ai Mori - Влюбилась (руки на шее) [feat. Ai Mori].mp3");
+    /****************/
+    audio3[0].pause();
+    audio3[0].load(); //suspends and restores all audio element
   });
-  $('.m_classic').hover(function() {
+  $('.m_classic').click(function() {
     $('#main').css({
       "background-image": "url('media/m_classic.jpg')",
       "transition-delay": "0.5s"
     });
+    //Смена треков
+    //Истоничк первой песни
+    $("#source1").attr("src", "audio/classic/Звери - Районы-кварталы.mp3");
+    /****************/
+    audio1[0].pause();
+    audio1[0].load(); //suspends and restores all audio element
+    //Истоничк второй песни
+    $("#source2").attr("src", "audio/classic/Звери - Рома, извини.mp3");
+    /****************/
+    audio2[0].pause();
+    audio2[0].load(); //suspends and restores all audio element
+    //Истоничк третьей песни
+    $("#source3").attr("src", "audio/classic/Звери - Южная ночь.mp3");
+    /****************/
+    audio3[0].pause();
+    audio3[0].load(); //suspends and restores all audio element
   });
-  $('.m_indi').hover(function() {
+  $('.m_indi').click(function() {
     $('#main').css({
       "background-image": "url('media/m_indi.jpg')",
       "transition-delay": "0.5s"
     });
+    //Смена треков
+    //Истоничк первой песни
+    $("#source1").attr("src", "audio/indi/Sati Akura - Otome Dissection.mp3");
+    /****************/
+    audio1[0].pause();
+    audio1[0].load(); //suspends and restores all audio element
+    //Истоничк второй песни
+    $("#source2").attr("src", "audio/indi/Sati Akura - The Vampire.mp3");
+    /****************/
+    audio2[0].pause();
+    audio2[0].load(); //suspends and restores all audio element
+    //Истоничк третьей песни
+    $("#source3").attr("src", "audio/indi/Sati Akura[Vocaloid RUS] - HIBANA .mp3");
+    /****************/
+    audio3[0].pause();
+    audio3[0].load(); //suspends and restores all audio element
   });
-  $('.m_metal').hover(function() {
+  $('.m_metal').click(function() {
     $('#main').css({
       "background-image": "url('media/m_metal.jpg')",
       "transition-delay": "0.5s"
     });
+    //Смена треков
+    //Истоничк первой песни
+    $("#source1").attr("src", "audio/metal/Волшебники Изумрудного Города - Не вам меня судить.mp3");
+    /****************/
+    audio1[0].pause();
+    audio1[0].load(); //suspends and restores all audio element
+    //Истоничк второй песни
+    $("#source2").attr("src", "audio/metal/Волшебники Изумрудного Города - Не такой как все.mp3");
+    /****************/
+    audio2[0].pause();
+    audio2[0].load(); //suspends and restores all audio element
+    //Истоничк третьей песни
+    $("#source3").attr("src", "audio/metal/Волшебники Изумрудного Города - Устоять.mp3");
+    /****************/
+    audio3[0].pause();
+    audio3[0].load(); //suspends and restores all audio element
   });
